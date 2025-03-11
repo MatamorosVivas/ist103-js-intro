46617305ef0cb42c3271ad3bec4550ccadb5d29d
carlos <c.matamoros.v2@gmail.com>
2025-03-10 20:30:06 -0700

    carlos-class activity 2 - 10 march 2025 #1

--- js.js
+++ js.js
@@ -6,6 +6,15 @@ function clickHeading() {
 var firstname = 'Carlos'
 var lastname = 'Matamoros'
 var age = 19
-var gradute = false
+var graduate = false
 const nationality = 'peruvian'
+
+console.log('First name is ' + firstname);
+console.log('Last name is ' + lastname);
+console.log('Age is ' + age);
+console.log('Graduate is ' + graduate);
+console.log('Nationality is ' + nationality);
+
+
+
 
